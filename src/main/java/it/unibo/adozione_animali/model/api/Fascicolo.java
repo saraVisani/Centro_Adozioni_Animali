d1f0dfe29b914e0f4ba65bb20033db674bb39428
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-08-20 00:46:59 +0200

    Added the Fascicolo interface and the FascicoloDAO class

---
+++ src/main/java/it/unibo/adozione_animali/model/api/Fascicolo.java
@@ -0,0 +1,10 @@
+package it.unibo.adozione_animali.model.api;
+
+/**
+ * Represents the Fascicolo table in the database.
+ */
+public interface Fascicolo {
+
+    void deleteFascicolo(int codFascicolo);
+
+}
---
+++ src/main/java/it/unibo/adozione_animali/model/impl/FascicoloDAO.java
@@ -0,0 +1,27 @@
+package it.unibo.adozione_animali.model.impl;
+
+import it.unibo.adozione_animali.model.api.Fascicolo;
+import nu.studer.sample.Tables;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class FascicoloDAO implements Fascicolo {
+
+    Logger logger = Logger.getLogger("loggerFascicolo");
+
+    @Override
+    public void deleteFascicolo(final int codFascicolo) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.deleteFrom(Tables.FASCICOLO_SANITARIO)
+                    .where(Tables.FASCICOLO_SANITARIO.COD_FASCICOLO.eq(codFascicolo))
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+}
