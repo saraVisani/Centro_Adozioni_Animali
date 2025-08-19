0d457922c9ed14df028f42bdb4bfbd6d00ed56db
Sara Visani <sara.visani7@studio.unibo.it>
2025-08-19 18:13:28 +0200

    Fixed config

--- src/main/java/it/unibo/adozione_animali/model/impl/DBConfig.java
+++ src/main/java/it/unibo/adozione_animali/model/impl/DBConfig.java
@@ -5,9 +5,14 @@ import java.sql.SQLException;
 import java.sql.Connection;
 
 public class DBConfig {
+
     public static final String URL = "jdbc:mysql://localhost:3306/adozione_animali?ssl=false";
     public static final String USER = "root";
     public static final String PASSWORD = "";
+
+    private DBConfig() {
+        // Private constructor to prevent instantiation
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
