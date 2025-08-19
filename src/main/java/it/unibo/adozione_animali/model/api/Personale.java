abe8565ace1467936fa39b92d86a0cba59471a2e
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-08-20 00:48:36 +0200

    Added the Personale interface and the PersonaleDAO class

---
+++ src/main/java/it/unibo/adozione_animali/model/api/Personale.java
@@ -0,0 +1,32 @@
+package it.unibo.adozione_animali.model.api;
+
+import java.time.LocalDate;
+
+/**
+ * Represents the Personale table in the database.
+ */
+public interface Personale{
+
+    void insertPersonale(String CF, byte tempoLavoro, LocalDate dataAssunzioneDip, LocalDate dataAssunzioneVol,
+                         LocalDate dataFineLavoroDip, LocalDate dataFineLavoroVol, short stipendio,
+                         byte exVolontario, byte exDipendente, String codProvincia, String codCitta,
+                         int numero);
+
+    void updateTempoLavoro(String CF, byte tempoLavoro);
+
+    void updateDataAssunzioneDip(String CF, LocalDate dataAssunzioneDip);
+
+    void updateDataAssunzioneVol(String CF, LocalDate dataAssunzioneVol);
+
+    void updateDataFineLavoroDip(String CF, LocalDate dataFineLavoroDip);
+
+    void updateDataFineLavoroVol(String CF, LocalDate dataFineLavoroVol);
+
+    void updateStipendio(String CF, short stipendio);
+
+    void updateIndirizzoCentro(String CF, String codProvincia, String codCitta, int numero);
+
+    void deletePersonale(String CF);
+
+
+}
---
+++ src/main/java/it/unibo/adozione_animali/model/impl/PersonaDAO.java
@@ -0,0 +1,84 @@
+package it.unibo.adozione_animali.model.impl;
+
+import it.unibo.adozione_animali.model.api.Persona;
+import nu.studer.sample.Tables;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class PersonaDAO implements Persona {
+
+    Logger logger = Logger.getLogger("loggerPersona");
+
+    @Override
+    public void insertPersona(final String CF, final String nome, final String cognome, final String email,
+                              final String password, final String telefono) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.insertInto(Tables.PERSONA)
+                    .set(Tables.PERSONA.CF, CF)
+                    .set(Tables.PERSONA.NOME, nome)
+                    .set(Tables.PERSONA.COGNOME, cognome)
+                    .set(Tables.PERSONA.EMAIL, email)
+                    .set(Tables.PERSONA.PASSWORD_PERSONA, password)
+                    .set(Tables.PERSONA.N_TELEFONO, telefono)
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+
+    @Override
+    public void updateEmail(final String CF, final String email) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.update(Tables.PERSONA)
+                    .set(Tables.PERSONA.EMAIL, email)
+                    .where(Tables.PERSONA.CF.eq(CF))
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+
+    @Override
+    public void updateTelefono(final String CF, final String telefono) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.update(Tables.PERSONA)
+                    .set(Tables.PERSONA.N_TELEFONO, telefono)
+                    .where(Tables.PERSONA.CF.eq(CF))
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+
+    @Override
+    public void updatePassword(final String CF, final String password) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.update(Tables.PERSONA)
+                    .set(Tables.PERSONA.PASSWORD_PERSONA, password)
+                    .where(Tables.PERSONA.CF.eq(CF))
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+
+    @Override
+    public void deletePersona(final String CF) {
+        try (Connection conn = DBConfig.getConnection()) {
+            DSLContext create = DSL.using(conn);
+            create.deleteFrom(Tables.PERSONA)
+                    .where(Tables.PERSONA.CF.eq(CF))
+                    .execute();
+        } catch (SQLException e) {
+            this.logger.severe("La connessione non ha funzionato");
+        }
+    }
+}
