3cfd4a0716f8b54db33fac17fb68aa28ec89b8dc
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-08-22 01:35:14 +0200

    Added the InserimentoComponentePanel class

---
+++ src/main/java/it/unibo/adozione_animali/view/animale/InserimentoComponentePanel.java
@@ -0,0 +1,117 @@
+package it.unibo.adozione_animali.view.animale;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InserimentoComponentePanel extends JPanel {
+
+    //TODO --> add action listener for button
+    public InserimentoComponentePanel() {
+        setLayout(new BorderLayout());
+
+        JPanel insertPanel = new JPanel(new BorderLayout());
+        JPanel insertPanelGen = new JPanel();
+        insertPanel.add(insertPanelGen, BorderLayout.NORTH);
+        insertPanel.add(new JPanel(), BorderLayout.CENTER);
+        GroupLayout insertLayout = new GroupLayout(insertPanelGen);
+        insertPanelGen.setLayout(insertLayout);
+        JScrollPane insertScroll = new JScrollPane(insertPanel);
+        insertScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        insertScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+
+        JButton aggiungi = new JButton("Aggiungi");
+
+        JTextField cod_prov = new JTextField(2);
+        JTextField cod_citta = new JTextField(2);
+        JTextField numeroInd = new JTextField(5);
+        JTextField codAnimale = new JTextField(5);
+        JLabel codProv = new JLabel("Provincia del centro");
+        JLabel codCitta = new JLabel("Città del centro");
+        JLabel numAnimale = new JLabel("Civico del centro");
+        JLabel codAnim = new JLabel("Codice dell'animale");
+        JTextField cod_specie = new JTextField(2);
+        JTextField nome_razza = new JTextField(50);
+        JTextField percentuale = new JTextField(2);
+        JLabel codSpecie = new JLabel("Codice Specie Componente");
+        JLabel nomeRazza = new JLabel("Razza Componente");
+        JLabel perc = new JLabel("Percentuale Composizione");
+
+
+        insertLayout.setAutoCreateContainerGaps(true);
+        insertLayout.setAutoCreateGaps(true);
+        insertLayout.setHorizontalGroup(
+                insertLayout.createSequentialGroup()
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(codProv)
+                                .addComponent(codCitta)
+                                .addComponent(numAnimale)
+                                .addComponent(codAnim)
+                                .addComponent(codSpecie)
+                                .addComponent(nomeRazza)
+                                .addComponent(perc)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(cod_prov)
+                                .addComponent(cod_citta)
+                                .addComponent(numeroInd)
+                                .addComponent(codAnimale)
+                                .addComponent(cod_specie)
+                                .addComponent(nome_razza)
+                                .addComponent(percentuale)
+                                .addComponent(aggiungi)
+                        )
+        );
+        insertLayout.setVerticalGroup(
+                insertLayout.createSequentialGroup()
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(codProv)
+                                .addComponent(cod_prov)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(codCitta)
+                                .addComponent(cod_citta)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(numAnimale)
+                                .addComponent(numeroInd)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(codAnim)
+                                .addComponent(codAnimale)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(codSpecie)
+                                .addComponent(cod_specie)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(nomeRazza)
+                                .addComponent(nome_razza)
+                        )
+                        .addGroup(insertLayout.createParallelGroup()
+                                .addComponent(perc)
+                                .addComponent(percentuale)
+                        )
+                        .addComponent(aggiungi)
+        );
+
+        insertPanelGen.add(codProv);
+        insertPanelGen.add(cod_prov);
+        insertPanelGen.add(codCitta);
+        insertPanelGen.add(cod_citta);
+        insertPanelGen.add(numAnimale);
+        insertPanelGen.add(numeroInd);
+        insertPanelGen.add(codAnim);
+        insertPanelGen.add(codAnimale);
+        insertPanelGen.add(codSpecie);
+        insertPanelGen.add(cod_specie);
+        insertPanelGen.add(nomeRazza);
+        insertPanelGen.add(nome_razza);
+        insertPanelGen.add(perc);
+        insertPanelGen.add(percentuale);
+        insertPanelGen.add(aggiungi);
+
+
+        add(insertScroll, BorderLayout.CENTER);
+    }
+}
