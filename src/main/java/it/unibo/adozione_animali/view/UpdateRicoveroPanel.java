6f9efcc9f026053a7c4314ff56e3e68bce7f45bf
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2025-08-22 14:59:23 +0200

    Added the the UpdateRicoveroPanel class

---
+++ src/main/java/it/unibo/adozione_animali/view/UpdateRicoveroPanel.java
@@ -0,0 +1,91 @@
+package it.unibo.adozione_animali.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UpdateRicoveroPanel extends JPanel {
+    
+    JPanel updatePanel;
+    
+    public UpdateRicoveroPanel() {
+
+        setLayout(new BorderLayout());
+
+        updatePanel = new JPanel(new BorderLayout());
+        JPanel updatePanelMore = new JPanel(new BorderLayout());
+        JPanel updatePanelGen = new JPanel();
+        updatePanelMore.add(updatePanelGen, BorderLayout.NORTH);
+        updatePanelMore.add(new JPanel(), BorderLayout.CENTER);
+        GroupLayout updateLayout = new GroupLayout(updatePanelGen);
+        updatePanelGen.setLayout(updateLayout);
+        JScrollPane updateScroll = new JScrollPane(updatePanelMore);
+        updateScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        updateScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        updatePanel.add(updateScroll, BorderLayout.CENTER);
+
+
+        JButton update = new JButton("Aggiorna");
+        JLabel codFascicoloLabel = new JLabel("Codice Fascicolo");
+        JTextField codFascicoloField = new JTextField(5);
+        JLabel numeroPaginaLabel = new JLabel("Numero Pagina");
+        JTextField numeroPaginaField = new JTextField(3);
+        JLabel paragrafoLabel = new JLabel("Paragrafo Pagina");
+        JTextField paragrafoField = new JTextField(1);
+        JTextField data_ricovero = new JTextField(50);
+        JLabel dataRicovero = new JLabel("Nuova Data Fine Ricovero (aaaa-mm-dd)");
+
+        updateLayout.setAutoCreateContainerGaps(true);
+        updateLayout.setAutoCreateGaps(true);
+        updateLayout.setHorizontalGroup(
+                updateLayout.createSequentialGroup()
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(codFascicoloLabel)
+                                .addComponent(numeroPaginaLabel)
+                                .addComponent(paragrafoLabel)
+                                .addComponent(dataRicovero)
+                        )
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(codFascicoloField)
+                                .addComponent(numeroPaginaField)
+                                .addComponent(paragrafoField)
+                                .addComponent(data_ricovero)
+                                .addComponent(update)
+                        )
+        );
+        updateLayout.setVerticalGroup(
+                updateLayout.createSequentialGroup()
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(codFascicoloLabel)
+                                .addComponent(codFascicoloField)
+                        )
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(numeroPaginaLabel)
+                                .addComponent(numeroPaginaField)
+                        )
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(paragrafoLabel)
+                                .addComponent(paragrafoField)
+                        )
+                        .addGroup(updateLayout.createParallelGroup()
+                                .addComponent(dataRicovero)
+                                .addComponent(data_ricovero)
+                        )
+                        .addComponent(update)
+        );
+
+        updatePanelGen.add(codFascicoloField);
+        updatePanelGen.add(codFascicoloLabel);
+        updatePanelGen.add(numeroPaginaField);
+        updatePanelGen.add(numeroPaginaLabel);
+        updatePanelGen.add(paragrafoField);
+        updatePanelGen.add(paragrafoLabel);
+        updatePanelGen.add(dataRicovero);
+        updatePanelGen.add(data_ricovero);
+        updatePanelGen.add(update);
+
+
+
+        add(updatePanel, BorderLayout.CENTER);
+        
+    }
+}
